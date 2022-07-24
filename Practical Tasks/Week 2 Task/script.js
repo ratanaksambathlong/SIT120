ba54a797c5a987907b9ae19eeb860de9a86b7fa3
ratanaksambathlong <ratanaksambathlong@gmail.com>
2022-07-24 15:54:23 +1000

    small changes

---
+++ Practical Tasks/Week 2 Task/script.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { title: 'Viewed the video', checked: false },
+            { title: 'Interacted with the map', checked: false },
+            { title: 'Checked the date', checked: false },
+        ],
+    },
+});
